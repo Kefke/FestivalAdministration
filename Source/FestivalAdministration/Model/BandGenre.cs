28975ca8f662c97b57a9802a498516d689ccaa71
Kevin Aelterman <kevin.aelterman@student.howest.be>
2013-12-19 21:08:28 +0100

    Updated BandGenre.cs

--- Source/FestivalAdministration/Model/BandGenre.cs
+++ Source/FestivalAdministration/Model/BandGenre.cs
@@ -8,5 +8,29 @@ namespace FestivalAdministration.Model
 {
     class BandGenre
     {
+        private int _ID;
+
+        public int ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+
+        private int _bandID;
+
+        public int BandID
+        {
+            get { return _bandID; }
+            set { _bandID = value; }
+        }
+
+        private int _genreID;
+
+        public int GenreID
+        {
+            get { return _genreID; }
+            set { _genreID = value; }
+        }
+        
     }
 }
