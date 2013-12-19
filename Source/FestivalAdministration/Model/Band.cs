06517e3ce60347856ae269a0fb2a0986396bb987
Kevin Aelterman <kevin.aelterman@student.howest.be>
2013-12-19 22:18:39 +0100

    Added MySQL support for Band and TimeSlot

--- Source/FestivalAdministration/Model/Band.cs
+++ Source/FestivalAdministration/Model/Band.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -55,6 +56,164 @@ namespace FestivalAdministration.Model
         {
             get { return _Description; }
             set { _Description = value; }
+        }
+
+        private static ObservableCollection<Band> _Bands = null;
+
+        public static ObservableCollection<Band> GetBands()
+        {
+            // If _Band is null, create the Observable Collection
+            if (_Bands == null)
+            {
+                try
+                {
+                    // Create _Band
+                    _Bands = new ObservableCollection<Band>();
+
+                    // Get data
+                    DbDataReader reader = Database.GetData("SELECT * FROM band");
+                    foreach (DbDataRecord record in reader)
+                    {
+                        // Create new Band
+                        Band band = new Band();
+
+                        // Get ID
+                        if (DBNull.Value.Equals(record["ID"])) band.ID = -1;
+                        else band.ID = Convert.ToInt32(record["ID"]);
+
+                        // Get Name
+                        if (DBNull.Value.Equals(record["Name"])) band.Name = "";
+                        else band.Name = record["Name"].ToString();
+
+                        // Get Twitter
+                        if (DBNull.Value.Equals(record["Twitter"])) band.Twitter = "";
+                        else band.Twitter = record["Twitter"].ToString();
+
+                        // Get Facebook
+                        if (DBNull.Value.Equals(record["Facebook"])) band.Facebook = "";
+                        else band.Facebook = record["Facebook"].ToString();
+
+                        // Get Picture
+                        if (DBNull.Value.Equals(record["Picture"])) band.Picture = "";
+                        else band.Picture = record["Picture"].ToString();
+
+                        // Get Description
+                        if (DBNull.Value.Equals(record["Description"])) band.Description = "";
+                        else band.Description = record["Description"].ToString();
+
+                        // Add Band
+                        _Bands.Add(band);
+                    }
+                }
+
+                // Fail
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+
+                    // Clear Bands
+                    _Bands.Clear();
+                    _Bands = null;
+                }
+
+            }
+
+            // Return _Band
+            return _Bands;
+        }
+
+        public static void AddBand(string name, string twitter, string facebook, string picture, string description)
+        {
+            // If _Band is null, create the Observable Collection
+            if (_Bands == null) GetBands();
+
+            try
+            {
+                // Add to db
+                DbParameter param1 = Database.AddParameter("@name", name);
+                DbParameter param2 = Database.AddParameter("@twitter", twitter);
+                DbParameter param3 = Database.AddParameter("@facebook", facebook);
+                DbParameter param4 = Database.AddParameter("@picture", picture);
+                DbParameter param5 = Database.AddParameter("@description", description);
+                int affectedRows = Database.ModifyData("INSERT INTO band(Name, Twitter, Facebook, Picture, Description) VALUES(@name, @twitter, @facebook, @picture, @description)", param1, param2, param3, param4, param5);
+                if (affectedRows == 0) return;
+
+                // Get ID from db
+                int id = 0;
+                DbDataReader reader = Database.GetData("SELECT ID FROM band WHERE Name = @name AND Twitter = @twitter AND Facebook = @facebook AND Picture = @picture AND Description = @description", param1, param2, param3, param4, param5);
+                foreach (DbDataRecord record in reader)
+                {
+                    // Get ID
+                    if (DBNull.Value.Equals(record["ID"])) id = -1;
+                    else id = Convert.ToInt32(record["ID"]);
+                }
+
+                _Bands.Add(new Band() { ID = id, Name = name, Twitter = twitter, Facebook = facebook, Picture = picture, Description = description });
+            }
+
+            // Fail
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static void UpdateBand(int index, string name, string twitter, string facebook, string picture, string description)
+        {
+            // If _Band is null, create the Observable Collection
+            if (_Bands == null) GetBands();
+
+            try
+            {
+                // Update db
+                DbParameter param1 = Database.AddParameter("@id", _Bands[index].ID);
+                DbParameter param2 = Database.AddParameter("@name", name);
+                DbParameter param3 = Database.AddParameter("@twitter", twitter);
+                DbParameter param4 = Database.AddParameter("@facebook", facebook);
+                DbParameter param5 = Database.AddParameter("@picture", picture);
+                DbParameter param6 = Database.AddParameter("@description", description);
+                int affectedRows = Database.ModifyData("UPDATE band SET Name = @name, Twitter = @twitter, Facebook = @facebook, Picture = @picture, Description = @description WHERE id = @id", param1, param2, param3, param4, param5, param6);
+                if (affectedRows == 0) return;
+
+                // Update _Band
+                _Bands[index].Name = name;
+                _Bands[index].Twitter = twitter;
+                _Bands[index].Facebook = facebook;
+                _Bands[index].Picture = picture;
+                _Bands[index].Description= description;
+            }
+
+            // Fail
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static void DeleteBand(int index)
+        {
+            // If _Band is null, create the Observable Collection
+            if (_Bands == null) GetBands();
+
+            // Only execute if index is valid
+            if (_Bands.Count <= index) return;
+
+            try
+            {
+                // Add to db
+                DbParameter param = Database.AddParameter("@id", _Bands[index].ID);
+                int affectedRows = Database.ModifyData("DELETE FROM band WHERE id = @id", param);
+                if (affectedRows == 0) return;
+
+                // Update _Band
+                _Bands.RemoveAt(index);
+            }
+
+            // Fail
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
--- Source/FestivalAdministration/Model/TimeSlot.cs
+++ Source/FestivalAdministration/Model/TimeSlot.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -47,6 +49,157 @@ namespace FestivalAdministration.Model
         {
             get { return _endDate; }
             set { _endDate = value; }
+        }
+
+        private static ObservableCollection<TimeSlot> _TimeSlots = null;
+
+        public static ObservableCollection<TimeSlot> GetTimeSlots()
+        {
+            // If _TimeSlots is null, create the Observable Collection
+            if (_TimeSlots == null)
+            {
+                try
+                {
+                    // Create _Contactperson
+                    _TimeSlots = new ObservableCollection<TimeSlot>();
+
+                    // Get data
+                    DbDataReader reader = Database.GetData("SELECT * FROM timeslot");
+                    foreach (DbDataRecord record in reader)
+                    {
+                        // Create new TimeSlot
+                        TimeSlot timeslot = new TimeSlot();
+
+                        // Get ID
+                        if (DBNull.Value.Equals(record["ID"])) timeslot.ID = -1;
+                        else timeslot.ID = Convert.ToInt32(record["ID"]);
+
+                        // Get BandID
+                        if (DBNull.Value.Equals(record["BandID"])) timeslot.BandID = -1;
+                        else timeslot.BandID = Convert.ToInt32(record["BandID"]);
+
+                        // Get StageID
+                        if (DBNull.Value.Equals(record["StageID"])) timeslot.StageID = -1;
+                        else timeslot.StageID = Convert.ToInt32(record["StageID"]);
+
+                        // Get StartDate
+                        if (DBNull.Value.Equals(record["StartDate"])) timeslot.StartDate = new DateTime();
+                        else timeslot.StartDate = Convert.ToDateTime(record["StartDate"]);
+
+                        // Get EndDate
+                        if (DBNull.Value.Equals(record["EndDate"])) timeslot.EndDate = new DateTime();
+                        else timeslot.EndDate = Convert.ToDateTime(record["EndDate"]);
+
+                        // Add TimeSlot
+                        _TimeSlots.Add(timeslot);
+                    }
+                }
+
+                // Fail
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+
+                    // Clear TimeSlots
+                    _TimeSlots.Clear();
+                    _TimeSlots = null;
+                }
+
+            }
+
+            // Return _TimeSlots
+            return _TimeSlots;
+        }
+
+        public static void AddTimeSlot(int bandid, int stageid, DateTime startdate, DateTime enddate)
+        {
+            // If _TimeSlots is null, create the Observable Collection
+            if (_TimeSlots == null) GetTimeSlots();
+
+            try
+            {
+                // Add to db
+                DbParameter param1 = Database.AddParameter("@bandid", bandid);
+                DbParameter param2 = Database.AddParameter("@stageid", stageid);
+                DbParameter param3 = Database.AddParameter("@startdate", startdate);
+                DbParameter param4 = Database.AddParameter("@enddate", enddate);
+                int affectedRows = Database.ModifyData("INSERT INTO TimeSlot(BandID, StageID, StartDate, EndDate) VALUES(@bandid, @stageid, @startdate, @enddate)", param1, param2, param3, param4);
+                if (affectedRows == 0) return;
+
+                // Get ID from db
+                int id = 0;
+                DbDataReader reader = Database.GetData("SELECT ID FROM timeslot WHERE BandID = @bandid AND StageID = @stageid AND StartDate = @startdate AND EndDate = @enddate", param1, param2, param3, param4);
+                foreach (DbDataRecord record in reader)
+                {
+                    // Get ID
+                    if (DBNull.Value.Equals(record["ID"])) id = -1;
+                    else id = Convert.ToInt32(record["ID"]);
+                }
+
+                _TimeSlots.Add(new TimeSlot() { ID = id, BandID = bandid, StageID = stageid, StartDate = startdate, EndDate = enddate });
+            }
+
+            // Fail
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static void UpdateTimeSlot(int index, int bandid, int stageid, DateTime startdate, DateTime enddate)
+        {
+            // If _TimeSlots is null, create the Observable Collection
+            if (_TimeSlots == null) GetTimeSlots();
+
+            try
+            {
+                // Update db
+                DbParameter param1 = Database.AddParameter("@id", _TimeSlots[index].ID);
+                DbParameter param2 = Database.AddParameter("@bandid", bandid);
+                DbParameter param3 = Database.AddParameter("@stageid", stageid);
+                DbParameter param4 = Database.AddParameter("@startdate", startdate);
+                DbParameter param5 = Database.AddParameter("@enddate", enddate);
+                int affectedRows = Database.ModifyData("UPDATE timeslot SET BandID = @bandid, StageID = @stageid, StartDate = @startdate, EndDate = @enddate WHERE id = @id", param1, param2, param3, param4, param5);
+                if (affectedRows == 0) return;
+
+                // Update _TimeSlots
+                _TimeSlots[index].BandID = bandid;
+                _TimeSlots[index].StageID = stageid;
+                _TimeSlots[index].StartDate = startdate;
+                _TimeSlots[index].EndDate = enddate;
+            }
+
+            // Fail
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static void DeleteTimeSlot(int index)
+        {
+            // If _TimeSlots is null, create the Observable Collection
+            if (_TimeSlots == null) GetTimeSlots();
+
+            // Only execute if index is valid
+            if (_TimeSlots.Count <= index) return;
+
+            try
+            {
+                // Add to db
+                DbParameter param = Database.AddParameter("@id", _TimeSlots[index].ID);
+                int affectedRows = Database.ModifyData("DELETE FROM TimeSlot WHERE id = @id", param);
+                if (affectedRows == 0) return;
+
+                // Update _TimeSlots
+                _TimeSlots.RemoveAt(index);
+            }
+
+            // Fail
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
