9ebd5598fe82b5bb4f044d1f738dc51adabb4040
Kevin Aelterman <kevin.aelterman@student.howest.be>
2014-01-06 07:26:57 +0100

    Started with Metro App. Crashes at point where the bitmap is read.

---
+++ WindowsApp/Source/MetroApp/Models/Stage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroApp.Models
+{
+    public class Stage
+    {
+        private int _ID;
+
+        public int ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+
+        private string _Name;
+
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+    }
+}
